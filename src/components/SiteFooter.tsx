91958ab83063ac0e5dbdfd5f6b999385cb688c17
pourya <pouryathesaymon@gmail.com>
2026-08-19 00:14:07 +0330

    dist added for gh-pages

--- src/components/SiteFooter.tsx
+++
@@ -1,7 +0,0 @@
-export function SiteFooter() {
-  return (
-    <footer className="relative z-10 text-center pb-3 shrink-0 font-mono text-[9px] tracking-[0.2em] text-[#3a5568] uppercase">
-      © {new Date().getFullYear()} Pourya Sharifi
-    </footer>
-  );
-}
--- src/types.ts
+++
@@ -1,8 +0,0 @@
-export type RoomId = "about" | "skills" | "works" | "contact";
-
-export interface Point {
-  x: number;
-  y: number;
-}
-
-export type OpenHandler = (id: RoomId, origin: Point) => void;
